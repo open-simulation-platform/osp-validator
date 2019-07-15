db3be80f457cc222180542c4df9d50f53c83c6fd
Martin Rindarøy <martin.rindaroy@sintef.no>
2019-07-15 09:01:13 +0200

    Added unit tests for CommandLineRunner

--- src/main/java/com/opensimulationplatform/runner/CommandLineRunner.java
+++ src/main/java/com/opensimulationplatform/runner/CommandLineRunner.java
@@ -161,7 +161,7 @@ public class CommandLineRunner {
     }
   }
   
-  private static class ExitCodes {
+  static class ExitCodes {
     static final ExitCode SUCCESS = new ExitCode(0, "SUCCESS");
     static final ExitCode INVALID_CONFIGURATION = new ExitCode(1, "INVALID_CONFIGURATION");
     static final ExitCode INVALID_INPUT = new ExitCode(2, "INVALID_INPUT");
---
+++ src/test/java/com/opensimulationplatform/runner/CommandLineRunnerTest.java
@@ -0,0 +1,60 @@
+package com.opensimulationplatform.runner;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.io.IOException;
+
+import static com.opensimulationplatform.runner.CommandLineRunner.ExitCodes.*;
+import static org.junit.Assert.assertTrue;
+
+public class CommandLineRunnerTest {
+  
+  @Rule
+  public final ExpectedSystemExit exit = ExpectedSystemExit.none();
+  
+  @Rule
+  public TemporaryFolder tempFolder = new TemporaryFolder();
+  
+  @Test
+  public void canCheckInvalidConfiguration() {
+    exit.expectSystemExitWithStatus(INVALID_CONFIGURATION.getExitCode());
+    CommandLineRunner.main(new String[]{"--osp-ontology", "./src/test/resources/validator/osp.owl", "--cse-config", "./src/test/resources/validator/cse-config-invalid.json"});
+  }
+  
+  @Test
+  public void canCheckValidConfiguration() {
+    exit.expectSystemExitWithStatus(SUCCESS.getExitCode());
+    CommandLineRunner.main(new String[]{"--osp-ontology", "./src/test/resources/validator/osp.owl", "--cse-config", "./src/test/resources/validator/cse-config-valid.json"});
+  }
+  
+  @Test
+  public void exitIfConfigFileDoesNotExist() {
+    exit.expectSystemExitWithStatus(INVALID_INPUT.getExitCode());
+    CommandLineRunner.main(new String[]{"--osp-ontology", "./src/test/resources/validator/osp.owl", "--cse-config", "./i-do-not-exist.json"});
+  }
+  
+  @Test
+  public void exitIfOspOwlFileDoesNotExist() {
+    exit.expectSystemExitWithStatus(INVALID_INPUT.getExitCode());
+    CommandLineRunner.main(new String[]{"--osp-ontology", "./i-do-not-exist.owl", "--cse-config", "./src/test/resources/validator/cse-config-valid.json"});
+  }
+  
+  @Test
+  public void exitIfInvalidLogLevelIsSet() {
+    System.setProperty("msmi.validator.log.level", "invalid-level");
+    exit.expectSystemExitWithStatus(INVALID_LOG_LEVEL.getExitCode());
+    CommandLineRunner.main(new String[]{"--osp-ontology", "./src/test/resources/validator/osp.owl", "--cse-config", "./src/test/resources/validator/cse-config-valid.json"});
+  }
+  
+  @Test
+  public void canSaveGeneratedOwlFile() throws IOException {
+    File saveDir = tempFolder.newFolder();
+    exit.expectSystemExitWithStatus(SUCCESS.getExitCode());
+    exit.checkAssertionAfterwards(() -> assertTrue(new File(saveDir, "configuration.owl").exists()));
+    CommandLineRunner.main(new String[]{"--osp-ontology", "./src/test/resources/validator/osp.owl", "--cse-config", "./src/test/resources/validator/cse-config-valid.json", "--save", saveDir.getAbsolutePath()});
+  }
+}
