dd043f2b6a81b5042acb1e7ddf7647debcc4318b
KRIEI <kristoffer.eide@dnvgl.com>
2019-07-17 10:49:50 +0200

    Muliparty configuration

--- http/src/main/java/com/opensimulationplatform/servlet/ValidationServlet.java
+++ http/src/main/java/com/opensimulationplatform/servlet/ValidationServlet.java
@@ -3,6 +3,7 @@ package com.opensimulationplatform.servlet;
 import com.google.gson.GsonBuilder;
 import com.opensimulationplatform.validator.MsmiValidator;
 import org.eclipse.jetty.http.HttpStatus;
+import org.eclipse.jetty.server.Response;
 import org.semanticweb.owlapi.io.OWLObjectRenderer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +13,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -23,64 +25,79 @@ import java.util.List;
 import java.util.Map;
 
 public class ValidationServlet extends HttpServlet {
-  
+
   private static final Logger LOG = LoggerFactory.getLogger(ValidationServlet.class);
-  
+
   @Override
   protected void doGet(HttpServletRequest httpRequest, HttpServletResponse httpResponse) throws IOException {
-    Map<String, URI> queries = getQueries(httpRequest);
-    File ontology = new File(queries.get("ontology"));
-    File configuration = new File(queries.get("configuration"));
-    
-    MsmiValidator.Result result = MsmiValidator.validate(ontology, configuration);
-    
-    createHttpResponse(httpResponse, result);
+    try {
+      Map<String, URI> queries = getQueries(httpRequest);
+      File ontology = new File(queries.get("ontology"));
+      File configuration = new File(queries.get("configuration"));
+
+      MsmiValidator.Result result = MsmiValidator.validate(ontology, configuration);
+
+      createHttpResponse(httpResponse, result, null);
+    } catch (Exception e) {
+      LOG.error("Failure during doGet", e);
+      createHttpResponse(httpResponse, null, e);
+    }
   }
-  
+
+
   @Override
   protected void doPost(HttpServletRequest httpRequest, HttpServletResponse httpResponse) throws ServletException, IOException {
-    File ontology = new File(getURI(httpRequest.getParameter("ontology")));
-    File configuration = new File(getURI(httpRequest.getParameter("configuration")));
-  
-    MsmiValidator.Result result = MsmiValidator.validate(ontology, configuration);
-  
-    createHttpResponse(httpResponse, result);
+    try {
+      File ontology = new File(getURI(httpRequest.getParameter("ontology")));
+      File configuration = new File(getURI(httpRequest.getParameter("configuration")));
+
+      MsmiValidator.Result result = MsmiValidator.validate(ontology, configuration);
+
+      createHttpResponse(httpResponse, result, null);
+    } catch (Exception e) {
+      LOG.error("Failure during doPost", e);
+      createHttpResponse(httpResponse, null, e);
+    }
   }
-  
-  private void createHttpResponse(HttpServletResponse httpResponse, MsmiValidator.Result result) throws IOException {
+
+  private void createHttpResponse(HttpServletResponse httpResponse, MsmiValidator.Result result, Exception e) throws IOException {
     httpResponse.setContentType("application/json");
     httpResponse.setCharacterEncoding("UTF-8");
-    httpResponse.setStatus(HttpStatus.OK_200);
     httpResponse.addHeader("Access-Control-Allow-Origin", "*");
+
     PrintWriter writer = httpResponse.getWriter();
-    
-    ValidationServletResponse servletResponse = createServletResponse(result);
-    writer.println(new GsonBuilder().create().toJson(servletResponse));
-    
+    if (e != null) {
+      httpResponse.setStatus(HttpStatus.BAD_REQUEST_400);
+      writer.println(new GsonBuilder().create().toJson(e.getMessage()));
+    } else {
+      httpResponse.setStatus(HttpStatus.OK_200);
+      ValidationServletResponse servletResponse = createServletResponse(result);
+      writer.println(new GsonBuilder().create().toJson(servletResponse));
+    }
     writer.flush();
     writer.close();
   }
-  
+
   private ValidationServletResponse createServletResponse(MsmiValidator.Result result) {
     ValidationServletResponse response = new ValidationServletResponse();
-    
+
     if (result.isSuccess()) {
       response.setValid("true");
     } else {
       response.setValid("false");
-      
+
       List<String> explanations = new ArrayList<>();
       OWLObjectRenderer renderer = new ManchesterOWLSyntaxOWLObjectRendererImpl();
       result.getExplanations().forEach(axioms -> {
         axioms.forEach(axiom -> explanations.add(renderer.render(axiom.getAxiomWithoutAnnotations())));
       });
-      
+
       response.setExplanations(explanations);
     }
-    
+
     return response;
   }
-  
+
   private Map<String, URI> getQueries(HttpServletRequest request) {
     Map<String, URI> queries = new HashMap<>();
     for (String queryString : request.getQueryString().split("&")) {
@@ -91,7 +108,7 @@ public class ValidationServlet extends HttpServlet {
     }
     return queries;
   }
-  
+
   private URI getURI(String value) {
     try {
       LOG.debug("Attempting to parse: '" + value + "' as a URI...");
