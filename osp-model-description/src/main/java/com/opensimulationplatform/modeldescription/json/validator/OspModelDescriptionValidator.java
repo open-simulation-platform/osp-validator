f73a10b77d5e98f02397c9d5038f9b9f97a1ed39
Martin Rindarøy <martin.rindaroy@sintef.no>
2019-08-29 11:24:42 +0200

    Added some logging in OspModelDescriptionValidator.java

--- osp-model-description/src/main/java/com/opensimulationplatform/modeldescription/json/validator/OspModelDescriptionValidator.java
+++ osp-model-description/src/main/java/com/opensimulationplatform/modeldescription/json/validator/OspModelDescriptionValidator.java
@@ -6,6 +6,8 @@ import com.opensimulationplatform.core.owl.util.ontologyparser.OntologyParser;
 import com.opensimulationplatform.core.util.resource.Resources;
 import no.ntnu.ihb.fmi4j.modeldescription.fmi1.FmiModelDescription;
 import no.ntnu.ihb.fmi4j.modeldescription.fmi1.FmiScalarVariable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +18,8 @@ import static java.util.Objects.nonNull;
 
 
 public class OspModelDescriptionValidator {
+  private static final Logger LOG = LoggerFactory.getLogger(OspModelDescriptionValidator.class);
+  
   private static final List<Function<OspModelDescription, Boolean>> validations = new ArrayList<>();
   private static final OntologyContent ontologyContent;
   
@@ -32,10 +36,12 @@ public class OspModelDescriptionValidator {
   }
   
   public static Result validate(OspModelDescription modelDescription) {
+    LOG.debug("Validating osp model description: " + modelDescription.getName());
     return new Result(validations.stream().allMatch(validation -> validation.apply(modelDescription)));
   }
   
   private static boolean allVariablesInAllSocketsExistInFmiModelDescription(OspModelDescription modelDescription) {
+    LOG.trace("allVariablesInAllSocketsExistInFmiModelDescription");
     FmiModelDescription fmiModelDescription = modelDescription.getFmiModelDescription();
     if (nonNull(fmiModelDescription)) {
       FmiModelDescription.ModelVariables modelVariables = fmiModelDescription.getModelVariables();
@@ -44,7 +50,11 @@ public class OspModelDescriptionValidator {
         Stream<OspVariable> variables = socket.getVariables().values().stream();
         return variables.allMatch(variable -> {
           Stream<FmiScalarVariable> fmiVariables = fmiScalarVariables.stream();
-          return fmiVariables.anyMatch(fmiVariable -> fmiVariable.getName().equals(variable.getName()));
+          boolean anyMatch = fmiVariables.anyMatch(fmiVariable -> fmiVariable.getName().equals(variable.getName()));
+          if (!anyMatch) {
+            LOG.debug("Variable: " + socket.getName() + "." + variable.getName() + " does NOT exists in fmiModelDescription: " + fmiModelDescription.getModelName());
+          }
+          return anyMatch;
         });
       });
     } else {
@@ -53,6 +63,7 @@ public class OspModelDescriptionValidator {
   }
   
   private static boolean allVariablesInAllPlugsExistInFmiModelDescription(OspModelDescription modelDescription) {
+    LOG.trace("allVariablesInAllPlugsExistInFmiModelDescription");
     FmiModelDescription fmiModelDescription = modelDescription.getFmiModelDescription();
     if (nonNull(fmiModelDescription)) {
       FmiModelDescription.ModelVariables modelVariables = fmiModelDescription.getModelVariables();
@@ -60,8 +71,13 @@ public class OspModelDescriptionValidator {
       return modelDescription.getOspPlugs().stream().allMatch(plug -> {
         Stream<OspVariable> variables = plug.getVariables().values().stream();
         return variables.allMatch(variable -> {
+          LOG.debug("Checking if variable: " + plug.getName() + "." + variable.getName() + " exists in fmiModelDescription: " + fmiModelDescription.getModelName());
           Stream<FmiScalarVariable> fmiVariables = fmiScalarVariables.stream();
-          return fmiVariables.anyMatch(fmiVariable -> fmiVariable.getName().equals(variable.getName()));
+          boolean anyMatch = fmiVariables.anyMatch(fmiVariable -> fmiVariable.getName().equals(variable.getName()));
+          if (!anyMatch) {
+            LOG.debug("Variable: " + plug.getName() + "." + variable.getName() + " does NOT exists in fmiModelDescription: " + fmiModelDescription.getModelName());
+          }
+          return anyMatch;
         });
       });
     } else {
@@ -70,10 +86,14 @@ public class OspModelDescriptionValidator {
   }
   
   private static boolean allBondsHaveUniqueNames(OspModelDescription modelDescription) {
+    LOG.trace("allBondsHaveUniqueNames");
     List<OspBond> ospBonds = modelDescription.getOspBonds();
     for (int i = 0; i < ospBonds.size(); i++) {
       for (int j = i + 1; j < ospBonds.size(); j++) {
-        if (ospBonds.get(i).getName().equals(ospBonds.get(j).getName())) {
+        String nameA = ospBonds.get(i).getName();
+        String nameB = ospBonds.get(j).getName();
+        if (nameA.equals(nameB)) {
+          LOG.debug("Bond " + nameA + " does not have a unique name");
           return false;
         }
       }
@@ -82,10 +102,14 @@ public class OspModelDescriptionValidator {
   }
   
   private static boolean allPlugsHaveUniqueNames(OspModelDescription modelDescription) {
+    LOG.trace("allPlugsHaveUniqueNames");
     List<OspPlug> ospPlugs = modelDescription.getOspPlugs();
     for (int i = 0; i < ospPlugs.size(); i++) {
       for (int j = i + 1; j < ospPlugs.size(); j++) {
-        if (ospPlugs.get(i).getName().equals(ospPlugs.get(j).getName())) {
+        String nameA = ospPlugs.get(i).getName();
+        String nameB = ospPlugs.get(j).getName();
+        if (nameA.equals(nameB)) {
+          LOG.debug("Plug " + nameA + " does not have a unique name");
           return false;
         }
       }
@@ -94,10 +118,14 @@ public class OspModelDescriptionValidator {
   }
   
   private static boolean allSocketsHaveUniqueNames(OspModelDescription modelDescription) {
+    LOG.trace("allSocketsHaveUniqueNames");
     List<OspSocket> ospSockets = modelDescription.getOspSockets();
     for (int i = 0; i < ospSockets.size(); i++) {
       for (int j = i + 1; j < ospSockets.size(); j++) {
-        if (ospSockets.get(i).getName().equals(ospSockets.get(j).getName())) {
+        String nameA = ospSockets.get(i).getName();
+        String nameB = ospSockets.get(j).getName();
+        if (nameA.equals(nameB)) {
+          LOG.debug("Socket " + nameA + " does not have a unique name");
           return false;
         }
       }
@@ -106,31 +134,58 @@ public class OspModelDescriptionValidator {
   }
   
   private static boolean allPlugsAndSocketsInBondsAreDefinedInOspModelDescription(OspModelDescription modelDescription) {
+    LOG.trace("allPlugsAndSocketsInBondsAreDefinedInOspModelDescription");
     return modelDescription.getOspBonds().stream().allMatch(bond -> allPlugsDefined(bond, modelDescription) && allSocketsDefined(bond, modelDescription));
   }
   
   private static boolean allPlugsDefined(OspBond ospBond, OspModelDescription modelDescription) {
+    LOG.trace("allPlugsDefined");
     return ospBond.getOspPlugs().stream().allMatch(plug -> plugIsDefinedInModelDescription(modelDescription, plug));
   }
   
   private static boolean allSocketsDefined(OspBond ospBond, OspModelDescription modelDescription) {
+    LOG.trace("allSocketsDefined");
     return ospBond.getOspSockets().stream().allMatch(socket -> socketIsDefinedInModelDescription(modelDescription, socket));
   }
   
   private static boolean plugIsDefinedInModelDescription(OspModelDescription modelDescription, OspPlug ospPlug) {
-    return modelDescription.getOspPlugs().stream().anyMatch(p -> p.getName().equals(ospPlug.getName()));
+    LOG.trace("plugIsDefinedInModelDescription");
+    boolean anyMatch = modelDescription.getOspPlugs().stream().anyMatch(p -> p.getName().equals(ospPlug.getName()));
+    if (!anyMatch) {
+      LOG.debug("Plug " + ospPlug.getName() + " is not defined in osp model description " + modelDescription.getName());
+    }
+    return anyMatch;
   }
   
   private static boolean socketIsDefinedInModelDescription(OspModelDescription modelDescription, OspSocket ospSocket) {
-    return modelDescription.getOspSockets().stream().anyMatch(p -> p.getName().equals(ospSocket.getName()));
+    LOG.trace("socketIsDefinedInModelDescription");
+    boolean anyMatch = modelDescription.getOspSockets().stream().anyMatch(p -> p.getName().equals(ospSocket.getName()));
+    if (!anyMatch) {
+      LOG.debug("Socket " + ospSocket.getName() + " is not defined in osp model description " + modelDescription.getName());
+    }
+    return anyMatch;
   }
   
   private static boolean allPlugTypesAreContainedInOntology(OspModelDescription ospModelDescription) {
-    return ospModelDescription.getOspPlugs().stream().allMatch(plug -> ontologyContent.getClasses().containsKey(plug.getType()));
+    LOG.trace("allPlugTypesAreContainedInOntology");
+    return ospModelDescription.getOspPlugs().stream().allMatch(plug -> {
+      boolean containsKey = ontologyContent.getClasses().containsKey(plug.getType());
+      if (!containsKey) {
+        LOG.debug("Plug type " + plug.getType() + " in plug " + plug.getName() + " is not defined in osp model description " + ospModelDescription.getName());
+      }
+      return containsKey;
+    });
   }
   
   private static boolean allSocketTypesAreContainedInOntology(OspModelDescription ospModelDescription) {
-    return ospModelDescription.getOspSockets().stream().allMatch(socket -> ontologyContent.getClasses().containsKey(socket.getType()));
+    LOG.trace("allSocketTypesAreContainedInOntology");
+    return ospModelDescription.getOspSockets().stream().allMatch(socket -> {
+      boolean containsKey = ontologyContent.getClasses().containsKey(socket.getType());
+      if (!containsKey) {
+        LOG.debug("Socket type " + socket.getType() + " in socket " + socket.getName() + " is not defined in osp model description " + ospModelDescription.getName());
+      }
+      return containsKey;
+    });
   }
   
   public static class Result {
