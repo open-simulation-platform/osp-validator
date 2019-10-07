c71be9b730fbd717a1ace125d68c9a07b879728b
Martin Rindarøy <martin.rindaroy@sintef.no>
2019-10-07 09:40:18 +0200

    Renamed from 'cse-config-x' to 'osp-system-structure-x'

--- cse-config-http/src/test/java/com/opensimulationplatform/cseconfig/http/TestResources.java
+++
@@ -1,9 +0,0 @@
-package com.opensimulationplatform.cseconfig.http;
-
-import com.opensimulationplatform.core.util.resource.Resource;
-
-class TestResources {
-  public static final Resource SYSTEM_STRUCTURE_VALID_XML = new Resource("/validator/xml/OspSystemStructure-valid.xml");
-  public static final Resource SYSTEM_STRUCTURE_INVALID_XML = new Resource("/validator/xml/OspSystemStructure-invalid.xml");
-  static final Resource OSP_OWL =  new Resource("/osp.owl");
-}
---
+++ osp-system-structure-http/src/test/java/com/opensimulationplatform/cseconfig/http/TestResources.java
@@ -0,0 +1,9 @@
+package com.opensimulationplatform.cseconfig.http;
+
+import com.opensimulationplatform.core.util.resource.Resource;
+
+class TestResources {
+  static final Resource SYSTEM_STRUCTURE_VALID_XML = new Resource("/validator/xml/OspSystemStructure-valid.xml");
+  static final Resource SYSTEM_STRUCTURE_INVALID_XML = new Resource("/validator/xml/OspSystemStructure-invalid.xml");
+  static final Resource OSP_OWL =  new Resource("/osp.owl");
+}
