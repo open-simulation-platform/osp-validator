089ae79317b8f7b986f6c076785f18925efcc012
Martin Rindarøy <martin.rindaroy@sintef.no>
2020-04-27 13:40:49 +0200

    Deleting *-cli modules

--- osp-model-description-cli/src/test/java/com/opensimulationplatform/modeldescription/cli/TestResources.java
+++
@@ -1,9 +0,0 @@
-package com.opensimulationplatform.modeldescription.cli;
-
-import java.io.File;
-
-public class TestResources {
-  static final File CRANE_CONTROLLER_FMU = new File("./src/test/resources/CraneController.fmu");
-  static final File CRANE_CONTROLLER_VALID = new File("./src/test/resources/CraneController_OspModelDescription-valid.xml");
-  static final File CRANE_CONTROLLER_INVALID = new File("./src/test/resources/CraneController_OspModelDescription-invalid.xml");
-}
--- osp-system-structure-cli/src/test/java/com/opensimulationplatform/systemstructure/cli/TestResources.java
+++
@@ -1,8 +0,0 @@
-package com.opensimulationplatform.systemstructure.cli;
-
-import java.io.File;
-
-class TestResources {
-  static final File SYSTEM_STRUCTURE_INVALID_XML = new File("./src/test/resources/validator/xml/OspSystemStructure-invalid.xml");
-  static final File SYSTEM_STRUCTURE_VALID_XML = new File("./src/test/resources/validator/xml/OspSystemStructure-valid.xml");
-}
