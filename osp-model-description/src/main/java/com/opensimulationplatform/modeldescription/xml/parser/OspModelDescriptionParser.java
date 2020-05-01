011501e0fb54c5f4c09c372aa094792605d93693
Martin Rindarøy <martin.rindaroy@sintef.no>
2020-05-01 15:57:28 +0200

    OspModelDescriptionParser now also has a LocationListener

--- osp-model-description/src/main/java/com/opensimulationplatform/modeldescription/xml/parser/OspModelDescriptionParser.java
+++ osp-model-description/src/main/java/com/opensimulationplatform/modeldescription/xml/parser/OspModelDescriptionParser.java
@@ -6,27 +6,58 @@ import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBIntrospector;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.stream.Location;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamReader;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 public class OspModelDescriptionParser {
+
+  private LocationListener listener;
+
   public OspModelDescriptionType parse(File ospModelDescriptionFile) {
-    try (InputStreamReader reader = new InputStreamReader(new FileInputStream(ospModelDescriptionFile), StandardCharsets.UTF_8)) {
-      SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+    XMLInputFactory inputFactory = XMLInputFactory.newFactory();
+    SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+    try (FileInputStream fileInputStream = new FileInputStream(ospModelDescriptionFile)) {
+      XMLStreamReader reader = inputFactory.createXMLStreamReader(fileInputStream);
+      listener = new LocationListener(reader);
+
       Schema schema = schemaFactory.newSchema(OspModelDescriptionParser.class.getResource("/OspModelDescription.xsd"));
 
       JAXBContext context = JAXBContext.newInstance(OspModelDescriptionType.class.getPackage().getName());
+
       Unmarshaller unmarshaller = context.createUnmarshaller();
       unmarshaller.setSchema(schema);
+      unmarshaller.setListener(listener);
 
       return (OspModelDescriptionType) JAXBIntrospector.getValue(unmarshaller.unmarshal(reader));
     } catch (Exception e) {
       String message = "Unable to parse '" + ospModelDescriptionFile.getAbsolutePath() + "'";
       throw new RuntimeException(message, e);
+    }
+  }
+
+  public Map<Object, Location> getLocations() {
+    return this.listener.locations;
+  }
+
+  private static class LocationListener extends Unmarshaller.Listener {
+    private final Map<Object, Location> locations;
+    private final XMLStreamReader reader;
+
+    public LocationListener(XMLStreamReader reader) {
+      this.reader = reader;
+      this.locations = new HashMap<>();
+    }
+
+    @Override
+    public void beforeUnmarshal(Object target, Object parent) {
+      locations.put(target, reader.getLocation());
     }
   }
 }
