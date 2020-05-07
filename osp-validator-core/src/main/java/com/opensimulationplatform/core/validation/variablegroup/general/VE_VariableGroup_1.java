46f51bbf2778be251920a4bae656bfd502110238
Martin Rindarøy <martin.rindaroy@sintef.no>
2020-05-07 10:16:29 +0200

    Including VE_VariableGroup_1 and VE_VariableGrou_2

---
+++ osp-validator-core/src/main/java/com/opensimulationplatform/core/validation/variablegroup/general/VE_VariableGroup_1.java
@@ -0,0 +1,26 @@
+package com.opensimulationplatform.core.validation.variablegroup.general;
+
+import com.opensimulationplatform.core.model.modeldescription.variablegroup.VariableGroup;
+import com.opensimulationplatform.core.validation.ValidationError;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLNamedIndividual;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.opensimulationplatform.gen.owl.model.OntologyClasses.VE_VariableGroup_1;
+
+public class VE_VariableGroup_1 extends ValidationError<VariableGroup> {
+  @Override
+  protected List<VariableGroup> getInvalidObjects() {
+    OWLClass validationErrorClass = context.owl.dataFactory.getOWLClass(VE_VariableGroup_1, context.owl.prefixManager);
+    Set<OWLNamedIndividual> invalidIndividuals = context.invalidIndividuals.get(validationErrorClass);
+    return invalidIndividuals.stream().map(individual -> context.variableGroups.get(individual)).collect(Collectors.toList());
+  }
+
+  @Override
+  protected String getErrorMessage(VariableGroup variableGroup) {
+    return "VariableGroup '" + variableGroup.getName().getId().get() + "' contains invalid variables";
+  }
+}
